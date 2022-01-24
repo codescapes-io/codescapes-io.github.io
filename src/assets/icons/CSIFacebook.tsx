b7d96db0405a40d4cb27974abba731f1ad22fc66
Ibram Gusti <62697975+ibramgusti@users.noreply.github.com>
2022-01-24 09:22:38 +0700

    Create home page (#4)
    * add some libraries such as axios and react-router
    
    * style for landing page
    
    * add icons
    
    * add some image
    
    * add home section/view
    
    * add navbar and footer component
    
    * create landing page, still progress
    
    * update for get api endpoint and HomePage interface
    
    * separate style each component/view
    
    * change Link to NavLink for active link style
    
    * add env variable for api homepage endpoint
    
    * update env variable and remove/comment it from gitignore

---
+++ src/assets/icons/CSIFacebook.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CSIFacebook = () => {
+    return (
+        <svg width="9" height="20" viewBox="0 0 9 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" clipRule="evenodd" d="M8.57649 9.70542L8.974 5.90787L5.82948 5.9309C5.76914 4.92484 5.75493 4.31731 6.14889 3.70978C6.75402 3.32188 8.16303 3.42639 8.94562 3.43524V0.126616C6.82144 -0.139068 3.66808 -0.151492 2.47201 1.89428C1.7675 3.10934 1.9822 4.35629 1.90944 5.76442L0.0053073 5.92917L0 9.62039L1.73906 9.72136L1.81185 19.1302H5.79937V9.70363H8.57649V9.70542Z" fill="#606060" />
+        </svg>
+
+    );
+};
+
+export default CSIFacebook;
---
+++ src/component/CSNavbar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    NavLink
+} from "react-router-dom";
+import Docs from '../view/Docs';
+import HomePage from '../view/CSHomePage'
+
+const Navbar: React.FC = () => {
+    return (
+        <Router>
+            <nav>
+                <div className='logo'>
+                    <img src="/logo-bve-light.png" alt="logo-bve" />
+                </div>
+                <ul>
+                    <li><NavLink to="/">HOME</NavLink></li>
+                    <li><NavLink to="/docs">DOCS</NavLink></li>
+                    <li><NavLink to="/pricing">PRICING</NavLink></li>
+                    <li><NavLink to="/blog">BLOG</NavLink></li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/docs' element={<Docs />} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default Navbar
